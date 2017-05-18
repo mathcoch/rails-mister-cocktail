4f7f74c31590d0d8c0f31fbc775a3aabb1bf007d
mathcoch <mathieu@cochet.me>
2017-05-18 12:45:21 +0200

    ongoing controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,28 @@
 class DosesController < ApplicationController
+  def create
+    @dose = Dose.new(dose_params)
+    ingredient = Ingredient.find_by_name(params[:ingredient_name])
+    @dose.ingredient = ingredient
+    @dose.cocktail = current_cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(current_cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def current_cocktail
+    Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:new, :create, :index, :show] do
-    resources :doses, only: [:new, :create]
+    resources :doses, only: [:create]
   end
 
   resources :doses, only: [:destroy]
